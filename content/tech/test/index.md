35eaef940d09455149eef0668391bbbf4d00cedf
unknown <pmck@mailbox.org>
2021-10-05 20:08:02 +1100

    improved test-network-viz to better handle update pattern and force layout

--- content/tech/test/index.md
+++ content/tech/test/index.md
@@ -16,24 +16,30 @@ Just for testing d3 visualisations, for now ...
 
 ---
 
-###### Network diagram (basic)
+###### Network diagram
+
+Preparing for the global lithium trade data visualisation. Objectives include:
+- Read in JSON data and draw graph
+- Accept input to change underlying data
+- _Smoothly_ transition from one state to the next
+- Position nodes geographically, but prevent their radii from overlapping
 
 <div>
     <input type='range', id='test-net-year-input', min='2015', max='2016', step='1'>
-    <!-- li-trade-network.js also handles updating the displayed year -->
-    <div id='test-net-year-input-display'></div>
 </div>
-<!-- <div id='netviz'></div> -->
+
 <div id='test-network-fig'></div>
 
-<!-- Calling the script to populate #test-network -->
-<!-- <script src="/scripts/test-network.js"></script><br> -->
-<script src="/scripts/test-network.js"></script><br>
+<!-- Call the script to populate #test-network-fig -->
+<script src="/scripts/test-network.js"></script>
 
+<br>
+
+The key to making this work was (a) understanding the d3 update pattern; (b) initialising nodes close to their eventual position, so that the force layout didn't jump in from the top-left corner every time.
 
 ---
 
-###### Bar chart (basic)
+<!-- ###### Bar chart (basic) -->
 <!-- Create a div where the graph will take place -->
 <!-- <div id='test-barchart-fig'></div> -->
 
--- static/scripts/test-network.js
+++ static/scripts/test-network.js
@@ -1,11 +1,12 @@
 
 const fig_width = 500
 const fig_height = 500
-
-let trade_year;
 let transition_duration = 500
 
-let roleScale = d3.scaleOrdinal(d3.schemeTableau10)
+let trade_year;
+let data = {nodes: null, links: null};
+d3.json('test-network-data.json').then((d) => {data = d; update_network()})
+
 
 // Expecting trade volumes to take arbitrary non-negative values, construct scales
 // for both bubbles (entity total volume) and links (pairwise volume)
@@ -21,6 +22,7 @@ let edgelinescaler = data => d3.scaleLinear()
     .domain([Math.min(...data.links.map(l => l.volume[trade_year])), Math.max(...data.links.map(l => l.volume[trade_year]))])
     .range([edgeline_min_size, edgeline_max_size]);
 
+let colourScale = d3.scaleOrdinal(d3.schemeTableau10)
 
 // Set up base selection and append a blank svg
 let fig = d3.select("#test-network-fig")
@@ -34,58 +36,67 @@ let force_simulation = d3.forceSimulation()
     .force("collision", d3.forceCollide().radius(d => 1.2*(d.r)))
     .force("x", d3.forceX().x(d => d.xpos))
     .force("y", d3.forceY().y(d => d.ypos))
-    // .force("link", d3.forceLink())
+    .on('tick', updateNetworkSim)
 
 function updateNetworkSim() {
-    d3.selectAll("circle.netnode")
-        // .attr("transform", d => `translate(${d.x},${d.y})`)
-        .attr("cx", d => d.xpos)
-        .attr("cy", d => d.ypos)
-    d3.selectAll("line.netlink")
-        .attr("x1", d => d.source.xpos)
-        .attr("x2", d => d.target.xpos)
-        .attr("y1", d => d.source.ypos)
-        .attr("y2", d => d.target.ypos)
-        // .attr("x1", d => d.source.x)
-        // .attr("x2", d => d.target.x)
-        // .attr("y1", d => d.source.y)
-        // .attr("y2", d => d.target.y)
+    d3.selectAll("circle.netnode2")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+    d3.selectAll("line.netlink2")
+        .attr("x1", d => d.source.x)
+        .attr("x2", d => d.target.x)
+        .attr("y1", d => d.source.y)
+        .attr("y2", d => d.target.y)
 }
 
+let node_data;
+let edge_data;
+let findNode;
 
-update_test_network_data = () => {
-    // Get the current input from the slider
+d3.select("#test-net-year-input").on('input', update_network)
+
+function update_network() {
+    // console.log(data)
     trade_year = d3.select('#test-net-year-input').node().value
-    // Handle the external display of this value
-    d3.selectAll('.test-year-display')
-        .remove()
-    d3.select('#test-net-year-input-display')
-        .append('text')
-        .attr('class', 'test-year-display')
-        .attr("text-anchor", "middle")
-        .text(trade_year)
-    
-    // Read data, then pass it to the update function
-    d3.json('test-network-data.json').then(update_test_network)
-}
 
-update_test_network = (data) => {
-    
-    // console.log(trade_year)
-    // console.log(data.nodes.map((i) => `${i.name}=${i.total[trade_year]}`))
-    
-    let global_total = data.nodes.reduce((a,x) => a + x.total[trade_year], 0)
+    nodescale = bubblescaler(data)
+    linkscale = edgelinescaler(data)
 
-    let bubblescale = bubblescaler(data)
-    let edgelinescale = edgelinescaler(data)
+    /**
+    For lines to appear below circles, they have to be drawn first. To avoid
+    drawing newly-appearing lines on top of already-existing circles, they'll
+    all get drawn at once.
+    
+    If any line/circle should _not_ be visible to the user, its thickness/radius
+    will simply be set to zero, rendering useless the enter() and exit() methods
+    in the d3 general update pattern.
 
-    // Construct array of nodes/bubbles
-    let nodeData = data.nodes.reduce((accum, node) => {
-        if(node.total[trade_year] > 0) {
+     */
+
+    node_data = data.nodes.reduce((accum, node) => {
+        // Check whether a circle already exists on the DOM for this node
+        // (see findNode below) and if so, pull across current (x,y) values
+        prevNode = findNode ? findNode[node.id] : undefined
+        if(prevNode) {
             accum.push({
                 id: node.id,
-                r: bubblescale(node.total[trade_year]),
+                r: node.total[trade_year] ? nodescale(node.total[trade_year]) : 0,
                 name: node.name,
+                x: prevNode.x,
+                y: prevNode.y,
+                xpos: node.xpos,
+                ypos: node.ypos
+            })
+        }
+        else {
+            // This node is newly introduced, so just use the
+            // canonical (x.y) positions as the start values
+            accum.push({
+                id: node.id,
+                r: node.total[trade_year] ? nodescale(node.total[trade_year]) : 0,
+                name: node.name,
+                x: node.xpos,
+                y: node.ypos,
                 xpos: node.xpos,
                 ypos: node.ypos
             })
@@ -93,53 +104,47 @@ update_test_network = (data) => {
         return accum
     }, [])
 
-    // Reset network forces to account for new node data
-    force_simulation
-        .alpha(1).restart()
-        .nodes(nodeData)
-        .on("tick", updateNetworkSim);
-
     // When updating the start and end points for drawing edge lines, we
     // will need access to the actual objects at the start and end nodes,
     // to fetch their current positions in the DOM.
-    // This is an array where indexing by a node's numeric ID will take
-    // you to a pointer for that node's object, to access its properties.
-    let findNode = nodeData.reduce((findarray, node) => {
+    findNode = node_data.reduce((findarray, node) => {
         findarray[node.id] = node;
         return findarray
     }, {})
+    
 
-    // Construct array of edges/lines
-    let edgeData = data.links.reduce((accum, link) => {
+    // Construct edge data similar to nodes, but edges only have volume
+    // when source and target nodes both have non-zero radius
+    edge_data = data.links.reduce((accum, link) => {
         let source_node = findNode[link.source]
         let target_node = findNode[link.target]
-        if(source_node && target_node) {
-            accum.push({
+        let show_edge = (source_node.r * target_node.r) > 0
+        accum.push({
                 id: `${link.source}-${link.target}`,
                 source: source_node,
                 target: target_node,
-                strength: edgelinescale(link.volume[trade_year])
+                strength: show_edge ? linkscale(link.volume[trade_year]) : 0
             })
-        }
         return accum
     }, [])
 
-    // Draw lines first so they appear beneath the nodes
-    let lineSelect = fig.selectAll("line.netlink")
-    lineSelect
-        .data(edgeData, d => d.id)
+
+    // Perform edge select-join-update before nodes and labels, so that
+    // graph lines appear underneath the bubbles and are hidden by them
+    
+    let edgeSelect = fig.selectAll("line.netlink2")
+        .data(edge_data, d => d.id)
         .join(
             (enter) => {
                 return enter
                     .append('line')
-                    .attr("class", "netlink")
-                    .style("stroke", "black")
-                    // initially at 0 to support transitions
+                    .attr('class', 'netlink2')
+                    .style('stroke', 'black')
                     .style('stroke-width', 0)
-                    .attr("x1", e => e.source.xpos)
-                    .attr("x2", e => e.target.xpos)
-                    .attr("y1", e => e.source.ypos)
-                    .attr("y2", e => e.target.ypos)
+                    .attr("x1", e => e.source.x)
+                    .attr("x2", e => e.target.x)
+                    .attr("y1", e => e.source.y)
+                    .attr("y2", e => e.target.y)
             },
             (update) => {
                 return update
@@ -149,34 +154,27 @@ update_test_network = (data) => {
                     .transition()
                     .duration(transition_duration)
                     .style('stroke-width', 0)
-                    // to keep lines underneath other objects,
-                    // don't actually remove them at exit
-                    // .remove()
+                    .remove()
             }
         )
         .transition()
         .duration(transition_duration)
-        .style('stroke-width', e => e.strength)
+        .style('stroke-width', d => d.strength)
 
 
-    // Separate nodes and node text, to avoid grouped join calls
-    let nodeSelect = fig.selectAll("circle.netnode")
-    nodeSelect
-        .data(nodeData, d => d.name)
+    let nodeSelect = fig.selectAll("circle.netnode2")
+        .data(node_data, d => d.name)
         .join(
             (enter) => {
-                // console.log('enter')
-                // console.log(enter)
                 return enter
                     .append('circle')
-                        .attr('class', 'netnode')
-                        .style('fill', (d, i) => roleScale(i))
-                        .style('opacity', 0) // for transitions
+                    .attr('class', 'netnode2')
+                    .attr('fill', d => colourScale(d.id))
             },
-            (update) => update,
+            (update) => {
+                return update
+            },
             (exit) => {
-                // console.log('exit')
-                // console.log(exit)
                 return exit
                     .transition()
                     .duration(transition_duration)
@@ -186,141 +184,37 @@ update_test_network = (data) => {
         )
         .transition()
         .duration(transition_duration)
-        .style('opacity', 1)
         .attr('r', d => d.r)
-    
-    textSelect = fig.selectAll('text.nodetext')
-    textSelect
-        .data(nodeData, d => `${d.name}-txt`)
-        .join(
-            (enter) => {
-                return enter
-                    .append('text')
-                        .attr('class', 'nodetext')
-                        .style("text-anchor", "middle")
-                        .style('opacity', 0)
-                        .attr("font-size", "0.8em")
-                        .attr("y", d => d.ypos + d.r + 15)
-                        .attr("x", d => d.xpos)
-                        .text(d => d.name)
-            },
-            (update) => update,
-            (exit) => {
-                return exit
-                    .transition()
-                    .duration(transition_duration)
-                    .style('opacity', 0)
-                    .remove()
-            }
-        )
-        .transition()
-        .duration(transition_duration)
-        .style('opacity', 1)
-        .attr("y", d => d.ypos + d.r + 15)
-        .attr("x", d => d.xpos)
 
+
+    // Reset the force simulation to take account of the new data
+    force_simulation.force('x').initialize(node_data)
+    force_simulation.force('y').initialize(node_data)
+    force_simulation.force('collision').initialize(node_data)
+    force_simulation.nodes(node_data)
+    force_simulation.alpha(0.8).restart();
 }
 
 
-update_test_network_data()
 
-d3.select("#test-net-year-input").on('input', update_test_network_data)
 
 
 
 /**
+ * Notes/references
  * 
  * 
- * Grouped join calls attempt ...
-
-    let nodeEnter = (enter) => {
-        enter.append('g').attr('class','nodegroup')
-
-        // .call(g => {console.log('enter'); console.log(g)})
-        .call(g => g
-            .append('circle')
-                .attr('class', 'netnode')
-                .style('fill', (d, i) => roleScale(i))
-                // .style('opacity', 0) // for transitions
-                // .transition().duration(transition_duration)
-                // .style('opacity', 1)
-                .attr('r', d => {console.log(`${d.name}: ${d.r}`); return d.r})
-        )
-        // .call(g => g
-        //     .selectAll('circle.netnode')
-        //     .transition().duration(transition_duration)
-        //     // .style('opacity', 1)
-        //     .attr('r', d => d.r)
-        // )
-        .call(g => g
-            .append('text')
-                .attr('class', 'nettext')
-                .attr('x', d => d.xpos + d.r + 5)
-                .attr('y', d => d.ypos + d.r + 5)
-                .attr('font-size', '0.6em')
-                .text(d => d.name)
-        )
-    }
-    // console.log(fig.selectAll('circle.netnode'))
-    let nodeUpdate = (update) => {
-        console.log(nodeData)
-        // console.log(update.call(g => g.selectAll('g.nodegroup.circle.netnode')))
-        // update.call(g => g.transition().duration(transition_duration))
-        update
-        .call(g => {console.log('update'); console.log(g.selectAll('circle.netnode'))})
-        update
-        .call(g => g
-            .selectAll('circle.netnode')
-            // .transition().duration(transition_duration)
-            .attr('r', d => {console.log(`${d.name}: ${d.r}`); return d.r})
-        )
-        .call(g => g
-            .selectAll('text.nettext')
-            // .transition().duration(transition_duration)
-            .attr('x', d => d.xpos + d.r + 5)
-            .attr('y', d => {console.log(d); return d.ypos + d.r + 5})
-        )
-    }
-
-    let nodeExit = (exit) => {
-        exit.call(g => g
-            .selectAll('circle.netnode')
-            .transition().duration(transition_duration)
-            .attr('r', 0)
-            .remove()
-        )
-        .call(g => g
-            .selectAll('text.nettext')
-            .transition().duration(transition_duration)
-            .style('opacity', 0)
-            .remove()
-        )
-        exit.call(g => g.remove())
-    }
+ * General update pattern in d3
+ * https://bl.ocks.org/mbostock/3808234
+ * 
+ * Modifying a force layout
+ * https://bl.ocks.org/mbostock/1095795
+ * 
+ * Fixing jumpiness with new data
+ * https://stackoverflow.com/questions/48087984/how-to-smoothly-update-a-d3-v4-force-diagram-with-new-data
+ * 
+ * Re-initialising forces w/ radius
+ * https://stackoverflow.com/questions/57277281/d3-how-to-update-force-simulation-when-data-values-change
+ * 
+ * 
  */
-
-
-
-    // // ~~ code from before finding the .join pattern ~~ //
-    // nodeSelect.attr('class', 'update')
-    //     .attr("r", d => d.r)
-    
-    // let nodeEnter = nodeSelect.enter()
-    //     .append("g")
-    //         .attr("class", "netnode");
-    // // console.log(nodeEnter)
-    // nodeEnter.append("circle")
-    //     .style("fill", (d, i) => roleScale(i))
-    //     .merge(nodeEnter)
-    //         .attr("r", d => d.r);
-
-    // nodeEnter.append("text")
-    //     .style("text-anchor", "middle")
-    //     .attr("y", d => d.r-2)
-    //     .attr("x", d => d.r+5)
-    //     .attr("font-size", "0.6em")
-    //     .text(d => d.name);
-    
-    // nodeSelect.exit()
-    //     .attr('class', 'exit')
-    //     .remove()
